fe5775b8514cc6966ab2d8b6b087767d19a5ecf4
Ethan Saxenian <esaxenian@middlebury.edu>
2022-09-05 21:28:19 -0400

    complete teams

--- helpers.py
+++ helpers.py
@@ -3,11 +3,17 @@ import json
 import requests
 from requests import Response
 
-API_BASE_URL = "https://statsapi.web.nhl.com/api/v1/"
+API_BASE_URL = "https://statsapi.web.nhl.com/api/v1"
+
+QueryArgs = list[tuple[str, str]]
 
 
-def fetch(endpoint: str) -> Response:
-    return requests.get(f"{API_BASE_URL}{endpoint}")
+def fetch(endpoint: str, query_args: QueryArgs = None, *paths) -> Response:
+    path_str = "/".join(paths)
+    query_str = "&".join([f"{k}={v}" for k, v in query_args])
+    url = f"{API_BASE_URL}/{endpoint}/{path_str}?{query_str}"
+    print(url)
+    return requests.get(url)
 
 
 def print_response(
--- nhl.py
+++ nhl.py
@@ -1,12 +1,11 @@
+from typing import Optional
+
 import typer
 
+from globals import PrettyFormat, SortKeys
 from helpers import fetch, print_response
 
 app = typer.Typer(name="nhl", help="An NHL API CLI", add_completion=False)
-
-PrettyFormat = typer.Option(False, "--pretty", help="Format output.")
-
-SortKeys = typer.Option(False, "--sort-keys", help="Sort output.")
 
 
 @app.command()
@@ -14,13 +13,41 @@ def teams(
     id: str = typer.Argument(
         "", help="The ID of the team.", show_default="Gets all teams."
     ),
+    expand: list[str] = typer.Option([], help="See nhl expands for details."),
+    season: Optional[str] = typer.Option(None, help="Specify the season."),
+    team_id: list[str] = typer.Option(
+        [], "--teamId", help="Can specify multiple team ids."
+    ),
+    roster: bool = typer.Option(
+        False,
+        "--roster",
+        help="Returns entire roster for a team including id value, name, jersey number and position details.",
+    ),
+    stats: bool = typer.Option(
+        False,
+        "--stats",
+        help="Returns current season stats and the current season rankings for a specific team.",
+    ),
     pretty: bool = PrettyFormat,
     sort_keys: bool = SortKeys,
 ):
     """
     Get information about NHL teams.
     """
-    res = fetch(f"teams/{id}")
+    query_params = []
+    for modifier in expand:
+        query_params.append(("expand", modifier))
+    if len(team_id) > 0:
+        query_params.append(("teamId", ",".join(team_id)))
+    if season is not None:
+        query_params.append(("season", season))
+
+    res = fetch(
+        "teams",
+        query_params,
+        id,
+        "roster" if roster else "stats" if stats else "",
+    )
     print_response(res, pretty=pretty, sort_keys=sort_keys)
 
 
