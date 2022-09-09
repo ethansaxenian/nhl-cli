e1490e49f7e01fc8650465660c189920176f3b98
Ethan Saxenian <esaxenian@middlebury.edu>
2022-09-08 22:05:27 -0400

    better docs generation script

--- generate-docs.sh
+++ generate-docs.sh
@@ -6,17 +6,20 @@ poetry remove typer black
 poetry add typer-cli
 
 # rich_help_panel throws an error when generating docs
-# so create a temporary copy of the cli and remove all occurances
-TEXT_TO_REMOVE1=', rich_help_panel="Configurations Commands"'
-TEXT_TO_REMOVE2='nhl\.'
-TEXT_TO_REPLACE2='tmp\.'
-cp -r nhl tmp
-grep -rl $TEXT_TO_REMOVE1 tmp | LC_ALL=C xargs sed -i "" "s/$TEXT_TO_REMOVE1//g"
-grep -rl $TEXT_TO_REMOVE2 tmp | LC_ALL=C xargs sed -i "" "s/$TEXT_TO_REMOVE2/$TEXT_TO_REPLACE2/g"
+# so create a temporary copy of the cli and remove all occurrences
+TMP_DIR=tmp
+cp -r nhl $TMP_DIR
+RICH_HELP_PANEL_TEXT='rich_help_panel="(.*)"'
+grep -rlE $RICH_HELP_PANEL_TEXT $TMP_DIR | LC_ALL=C xargs sed -Ei "" "s/$RICH_HELP_PANEL_TEXT//g"
+
+# refactor tmp module imports
+OLD_MODULE_IMPORT="^from nhl"
+TMP_MODULE_IMPORT="from $TMP_DIR"
+grep -rlE $OLD_MODULE_IMPORT $TMP_DIR | LC_ALL=C xargs sed -Ei "" "s/$OLD_MODULE_IMPORT/$TMP_MODULE_IMPORT/g"
 
 # generate the docs from the temporary cli and remove it
-typer tmp/main.py utils docs --name nhl --output DOCS.md
-rm -rf tmp
+typer $TMP_DIR/main.py utils docs --name nhl --output DOCS.md
+rm -rf $TMP_DIR
 
 # finally, uninstall typer-cli and reinstall typer and black with latest versions
 poetry remove typer-cli
